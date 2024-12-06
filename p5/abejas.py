03fec7acce75551082c20cff8898c14a2b4f9b7b
CatusElPoderoso <cactuselpoeroso@gmail.com>
2024-12-06 01:29:22 -0600

    random randint

--- p5/abejas.py
+++ p5/abejas.py
@@ -16,7 +16,7 @@ def poblacion_inicial(linf, lsup):
 # inicializamos las abejas
 def inicializa_abejas():
     abejas = []
-    for i in range(0, len(nobreras)): # 20 abejas
+    for i in range(0, nobreras): # 20 abejas
         aux = []
         # cada variable es una solución, son siete soluciones
         aux.append(poblacion_inicial(linf, lsup)) # sol 1
@@ -35,14 +35,21 @@ def fase_obrera():
     abejas = inicializa_abejas() # inicializamos la colonia de abejas (20)
     
     # generamos una nueva solución
-    for i in range(0, len(abejas)):
+    for i in range(0, nobreras):
+        abeja_random = random.randint(0, nobreras-1) # numero de la abeja aleatoria
+        while i == abeja_random:
+            abeja_random = random.randint(0, nobreras-1) # numero de la abeja aleatoria
         x = abejas[i] # extraemos la abeja con la que vamos a trabajar
-        abeja_random = random.uniform(0,len(abejas)) # numero de la abeja aleatoria
         k = abejas[abeja_random] # abeja aleatoria
-        
         # por cada abeja seleccionamos una fuente de alimento
-        j = random.uniform(0,6) # número aleatorio para modificar (posición)
+        j = random.randint(0,6) # número aleatorio para modificar (posición)
+        print("Posición modificada: " + str(j+1) + ", con la abeja número: " + str(abeja_random+1))
         vmodif = x[j] + random.uniform(-1,1) * (x[j] - k[j]) # variable modificada
         
+        # FALTA LA VALIDACIÓN DEL MINIMO DE LP Y SS
+        
         aux = x
-        aux[j] = vmodif # actualizamos la variable modificada en un auxiliar
+        aux[j] = round((vmodif),1) # actualizamos la variable modificada en un auxiliar
+        print("Abeja nueva: " + str(aux))
+
+fase_obrera()
